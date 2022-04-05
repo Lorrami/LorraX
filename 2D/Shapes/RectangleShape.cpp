e0f9ab933dff69ddada36a2e7479810566c88e19
lorrami <wwwromchelo040608@gmail.com>
2022-04-05 17:57:10 +0200

    RectangleShape fix

--- 2D/Shapes/RectangleShape.cpp
+++ 2D/Shapes/RectangleShape.cpp
@@ -1,1 +1,12 @@
 #include "RectangleShape.hpp"
+
+void RectangleShape::FindIntersection(DefaultShape *Object) {
+    if (this->Position.x + this->Origin.x > Object->Position.x - Object->Origin.x
+        && this->Position.y + this->Origin.y > Object->Position.y - Object->Origin.y
+
+        && this->Position.x - this->Origin.x < Object->Position.x + Object->Origin.x
+        && this->Position.y - this->Origin.y < Object->Position.y + Object->Origin.y)
+    {
+        this->IsCrashed = true;
+    }
+}
--- 2D/Shapes/RectangleShape.hpp
+++ 2D/Shapes/RectangleShape.hpp
@@ -8,12 +8,20 @@
 struct RectangleShape : public DefaultShape
 {
 public:
-	explicit RectangleShape(Vector2f Position = {30.0f}, Color FillColor = Color::White, Vector2f Size = {30.0f}, Vector2f Origin = {0.0f}, float Rotation = 0)
-	{
-		this->Position = Position;
-		this->FillColor = FillColor;
-		this->Size = Size;
-		this->Origin = Origin;
-		this->Rotation = Rotation;
-	}
+    explicit RectangleShape(Vector2f Position = {30.0f}, Color FillColor = Color::White, Vector2f Size = {30.0f}, Vector2f Origin = {0.0f}, float Rotation = 0) {
+        this->Position = Position;
+        this->FillColor = FillColor;
+        this->Size = Size;
+        this->Origin = Origin;
+        this->Rotation = Rotation;
+    }
+
+    Vector2f Position;
+    Color FillColor;
+    Vector2f Size;
+    Vector2f Origin;
+    float Rotation;
+
+    bool IsCrashed = false;
+    void FindIntersection(DefaultShape *Object) override;
 };
