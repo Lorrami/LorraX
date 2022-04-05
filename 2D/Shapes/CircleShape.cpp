f42a13a2892439ddccc266c571c7df6def13b3d3
lorrami <wwwromchelo040608@gmail.com>
2022-04-05 18:00:06 +0200

    CircleShape fix

--- 2D/Shapes/CircleShape.cpp
+++ 2D/Shapes/CircleShape.cpp
@@ -1,1 +1,12 @@
 #include "CircleShape.hpp"
+
+void CircleShape::FindIntersection(DefaultShape *Object) {
+    if (this->Position.x + this->Origin.x > Object->Position.x - Object->Origin.x
+        && this->Position.y + this->Origin.y > Object->Position.y - Object->Origin.y
+
+        && this->Position.x - this->Origin.x < Object->Position.x + Object->Origin.x
+        && this->Position.y - this->Origin.y < Object->Position.y + Object->Origin.y)
+    {
+        this->IsCrashed = true;
+    }
+}
